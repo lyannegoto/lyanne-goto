06c0b21acb187357a0423486152280b760709f7e
Bruno <brunorz@hotmail.com>
2021-06-28 01:19:10 -0300

    pic trio

--- js/carousel.js
+++ js/carousel.js
@@ -1,34 +1,51 @@
-const img = document.getElementById('carousel');
+const carousels = document.getElementsByClassName('carousel-img');
 const rightBtn = document.getElementById('right-btn');
 const leftBtn = document.getElementById('left-btn');
 
 let pictures = [
-    './images/mana/1.jpg',
-    './images/mana/2.jpg',
-    './images/mana/3.jpg',
+    ['./images/mana/1.jpg',
+        './images/mana/3.jpg',
+        './images/mana/2.jpg'],
+    ['./images/familia/2.jpg',
+        './images/familia/1.jpg',
+        './images/familia/3.jpg'],
+    ['./images/muie/1.jpg',
+        './images/muie/3.jpg',
+        './images/muie/2.jpg']
 ];
 
-img.src = pictures[0];
+carousels[0].src = pictures[0][0]
+carousels[1].src = pictures[0][1]
+carousels[2].src = pictures[0][2]
+
 let position = 0;
 
 const moveRight = () => {
     if (position >= pictures.length - 1) {
         position = 0
-        img.src = pictures[position];
+        for (let i = 0; i < 3; i++) {
+            carousels[i].src = pictures[position][i]
+        }
         return;
     }
-    img.src = pictures[position + 1];
     position++;
+    for (let i = 0; i < 3; i++) {
+        carousels[i].src = pictures[position][i]
+    }    
 }
 
 const moveLeft = () => {
     if (position < 1) {
-        position = pictures.length - 1;
-        img.src = pictures[position];
+        position = pictures.length - 1
+        for (let i = 0; i < 3; i++) {
+            carousels[i].src = pictures[position][i]
+        }
         return;
     }
-    img.src = pictures[position - 1];
     position--;
+    for (let i = 0; i < 3; i++) {
+        carousels[i].src = pictures[position][i]
+    }
 }
 
 rightBtn.addEventListener("click", moveRight);
